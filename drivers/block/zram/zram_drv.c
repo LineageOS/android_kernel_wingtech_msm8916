48daca819379a4ce9e0ff147a44836c93b0063a4
Olav Haugan <ohaugan@codeaurora.org>
2014-10-24 08:52:18 -0700

    zram: Rate limit memory allocation errors
    If an error occurs allocating memory for zram we will not be able to
    fullfill the request to store the page in zram. The swap subsystem still
    continues to try to swap out pages to zram even when this error occurs
    since there is currently no facility to stop the swap subsystem from
    swapping out during such errors. This can cause the system to be
    overflowed with logging errors.
    
    Reduce the amount of logging to prevent the kernel log from being filled
    with these error messages.
    
    Change-Id: I70b65b8db548a7b57a6ee7fc70cd23dd0f5f5d89
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -32,6 +32,7 @@
 #include <linux/string.h>
 #include <linux/vmalloc.h>
 #include <linux/err.h>
+#include <linux/ratelimit.h>
 
 #include "zram_drv.h"
 
@@ -39,6 +40,12 @@
 static int zram_major;
 static struct zram *zram_devices;
 static const char *default_compressor = "lzo";
+
+/*
+ * We don't need to see memory allocation errors more than once every 1
+ * second to know that a problem is occurring.
+ */
+#define ALLOC_ERROR_LOG_RATE_MS 1000
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
@@ -588,6 +595,7 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 	struct zcomp_strm *zstrm;
 	bool locked = false;
 	unsigned long alloced_pages;
+	static unsigned long zram_rs_time;
 
 	page = bvec->bv_page;
 	if (is_partial_io(bvec)) {
@@ -652,8 +660,10 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 
 	handle = zs_malloc(meta->mem_pool, clen);
 	if (!handle) {
-		pr_info("Error allocating memory for compressed page: %u, size=%zu\n",
-			index, clen);
+		if (printk_timed_ratelimit(&zram_rs_time,
+					   ALLOC_ERROR_LOG_RATE_MS))
+			pr_info("Error allocating memory for compressed page: %u, size=%zu\n",
+				index, clen);
 		ret = -ENOMEM;
 		goto out;
 	}
