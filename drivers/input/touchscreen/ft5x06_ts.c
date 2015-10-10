9ffc2f98247014e9e7fb504abdf47c3ef9239267
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-10-11 01:28:42 +0700

    input: ft5x06_ts: Don't perform resume if panel was not yet blanked
    * On startup the FB blank notifier receives 2 unblank notifications
      which the touchscreen should not respond to, since no corresponding
      blank notification has yet been issued.
    
    Based on Ethan Chen <intervigil@gmail.com> patch for gsl_ts
    
    Change-Id: Ib90e2132be3b7df7aa0063b5697cf032a6f101ec

--- drivers/input/touchscreen/ft5x06_ts.c
+++ drivers/input/touchscreen/ft5x06_ts.c
@@ -1286,6 +1286,7 @@ static int ft5x06_ts_resume(struct device *dev)
 #endif
 
 #if defined(CONFIG_FB)
+static bool unblanked_once = false;
 static int fb_notifier_callback(struct notifier_block *self,
 				 unsigned long event, void *data)
 {
@@ -1297,10 +1298,13 @@ static int fb_notifier_callback(struct notifier_block *self,
 	if (evdata && evdata->data && event == FB_EVENT_BLANK &&
 			ft5x06_data && ft5x06_data->client) {
 		blank = evdata->data;
-		if (*blank == FB_BLANK_UNBLANK)
-			ft5x06_ts_resume(&ft5x06_data->client->dev);
-		else if (*blank == FB_BLANK_POWERDOWN)
+		if (*blank == FB_BLANK_UNBLANK) {
+			if (unblanked_once)
+				ft5x06_ts_resume(&ft5x06_data->client->dev);
+		} else if (*blank == FB_BLANK_POWERDOWN) {
+			unblanked_once = true;
 			ft5x06_ts_suspend(&ft5x06_data->client->dev);
+		}
 	}
 
 	return 0;
