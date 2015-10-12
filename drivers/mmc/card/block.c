73e2ef49f02c4b345cf273c36f4cad7758197892
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-10-12 23:00:03 +0700

    mmc: card: Enable timeout quirk for all Numonyx MMC
    In attemp to fix data corruption issue
    
    Change-Id: I427b018d75e2b4086ce96a5d6541827366026d32

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -3276,6 +3276,8 @@ static const struct mmc_fixup blk_fixups[] =
 		  MMC_QUIRK_SEC_ERASE_TRIM_BROKEN),
 	MMC_FIXUP(CID_NAME_ANY, CID_MANFID_HYNIX, CID_OEMID_ANY, add_quirk_mmc,
 		  MMC_QUIRK_BROKEN_DATA_TIMEOUT),
+	MMC_FIXUP(CID_NAME_ANY, CID_MANFID_NUMONYX_MICRON, CID_OEMID_ANY,
+		  add_quirk_mmc, MMC_QUIRK_BROKEN_DATA_TIMEOUT),
 
 	/* Some INAND MCP devices advertise incorrect timeout values */
 	MMC_FIXUP("SEM04G", 0x45, CID_OEMID_ANY, add_quirk_mmc,
