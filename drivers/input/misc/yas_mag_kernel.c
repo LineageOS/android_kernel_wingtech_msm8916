b4e01fa7c30d33c1cb5a0f6711fc0f2246982a44
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-11-17 22:28:01 +0700

    input: misc: yas53x: Add sensors max delay time
    Max delay is the delay between two sensor events
    corresponding to the lowest frequency that this
    sensor supports. When lower frequencies are requested
    through batch()/setDelay() the events will be generated
    at this frequency instead. It can be used by the
    framework or applications to estimate when the batch
    FIFO may be full.
    
    Adapted from Daqing Chen <chendaqing@codeaurora.org> patch
    
    Change-Id: I1a5ba7211aaa8441f49e7f6d2ab4e56c7ff83e35

--- drivers/input/misc/yas_mag_kernel.c
+++ drivers/input/misc/yas_mag_kernel.c
@@ -105,6 +105,7 @@ static struct sensors_classdev sensors_cdev = {
 	.resolution = "1",
 	.sensor_power = "0.40",
 	.min_delay = 10000,
+	.max_delay = 10000,
 	.fifo_reserved_event_count = 0,
 	.fifo_max_event_count = 0,
 	.enabled = 0,
