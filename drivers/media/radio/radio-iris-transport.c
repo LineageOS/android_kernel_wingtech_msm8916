d5d677e0be61b918307e1dfe331daecb1eb395ba
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2016-05-14 14:17:33 +0700

    Revert "media: radio: IRIS transport without firmware loading support"
    This reverts commit 5768c019d0574604b00856b8f645cee948b6de08.
    
    Change-Id: I75421262f6469d960831dca3542c5c32c7bb2aed

--- drivers/media/radio/radio-iris-transport.c
+++ drivers/media/radio/radio-iris-transport.c
@@ -38,17 +38,12 @@ struct radio_data {
 	struct smd_channel  *fm_channel;
 };
 struct radio_data hs;
-#ifndef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
 static DEFINE_MUTEX(fm_smd_enable);
 static int fmsmd_set;
 static int hcismd_fm_set_enable(const char *val, struct kernel_param *kp);
 module_param_call(fmsmd_set, hcismd_fm_set_enable, NULL, &fmsmd_set, 0644);
-static void radio_hci_smd_deregister(void);
-#else
-int fmsmd_ready = -1;
-void radio_hci_smd_deregister(void);
-#endif
 static struct work_struct *reset_worker;
+static void radio_hci_smd_deregister(void);
 
 static void radio_hci_smd_destruct(struct radio_hci_dev *hdev)
 {
@@ -204,11 +199,7 @@ static int radio_hci_smd_register_dev(struct radio_data *hsmd)
 	return 0;
 }
 
-#ifdef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
-void radio_hci_smd_deregister(void)
-#else
 static void radio_hci_smd_deregister(void)
-#endif
 {
 	radio_hci_unregister_dev(hs.hdev);
 	kfree(hs.hdev);
@@ -216,23 +207,14 @@ static void radio_hci_smd_deregister(void)
 
 	smd_close(hs.fm_channel);
 	hs.fm_channel = 0;
-#ifdef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
-	fmsmd_ready = -1;
-#else
 	fmsmd_set = 0;
-#endif
 }
 
-#ifdef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
-int radio_hci_smd_init(void)
-#else
 static int radio_hci_smd_init(void)
-#endif
 {
 	return radio_hci_smd_register_dev(&hs);
 }
 
-#ifndef CONFIG_RADIO_IRIS_TRANSPORT_NO_FIRMWARE
 static void radio_hci_smd_exit(void)
 {
 	radio_hci_smd_deregister();
@@ -260,7 +242,6 @@ done:
 	mutex_unlock(&fm_smd_enable);
 	return ret;
 }
-#endif
 MODULE_DESCRIPTION("FM SMD driver");
 MODULE_AUTHOR("Ankur Nandwani <ankurn@codeaurora.org>");
 MODULE_LICENSE("GPL v2");
