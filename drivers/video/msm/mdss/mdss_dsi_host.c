783d962264e3b10e2bb1f0ae75c2723cf34905d7
nicknitewolf <nicknitewolf@lineageos.org>
2018-01-02 03:14:27 +0800

    Revert "msm: mdss: add support of border area"
    This reverts commit bdaa311e16084f539cf39870641ee686f1d80bbd.
    
    Change-Id: Iea464c0c48ca14e41968fa4137569e5ccb6d6a9b

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -1062,11 +1062,8 @@ static void mdss_dsi_mode_setup(struct mdss_panel_data *pdata)
 	height = pdata->panel_info.yres;
 
 	if (pdata->panel_info.type == MIPI_VIDEO_PANEL) {
-		dummy_xres = mult_frac((pdata->panel_info.lcdc.border_left +
-				pdata->panel_info.lcdc.border_right),
-				dst_bpp, pdata->panel_info.bpp);
-		dummy_yres = pdata->panel_info.lcdc.border_top +
-				pdata->panel_info.lcdc.border_bottom;
+		dummy_xres = pdata->panel_info.lcdc.xres_pad;
+		dummy_yres = pdata->panel_info.lcdc.yres_pad;
 	}
 
 	vsync_period = vspw + vbp + height + dummy_yres + vfp;
