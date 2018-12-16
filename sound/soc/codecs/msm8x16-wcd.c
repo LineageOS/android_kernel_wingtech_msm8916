1d52a6ef08a6df31d69bf27f7b4ffc05d44134c7
Ketut P. Kumajaya <ketut.kumajaya@gmail.com>
2018-12-16 07:25:09 +0000

    AsoC: msm: Changes to connect external headset to HPH
    Add widgets to provide a switch that connects external headset
    PA to HPH left or right. Enable the external PA GPIO in the
    dapm power up sequence when this switch connects external PA
    to HPH left or right.
    
    Change-Id: If398f3cd5e3ebae84587bc1481e8a82b2adc526e
    Signed-off-by: Ketut P. Kumajaya <ketut.kumajaya@gmail.com>

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -444,6 +444,16 @@ void msm8x16_wcd_spk_ext_pa_cb(
 	msm8x16_wcd->codec_spk_ext_pa_cb = codec_spk_ext_pa;
 }
 
+void msm8x16_wcd_hs_ext_pa_cb(
+		int (*codec_hs_ext_pa)(struct snd_soc_codec *codec,
+			int enable), struct snd_soc_codec *codec)
+{
+	struct msm8x16_wcd_priv *msm8x16_wcd = snd_soc_codec_get_drvdata(codec);
+
+	pr_debug("%s: Enter\n", __func__);
+	msm8x16_wcd->codec_hs_ext_pa_cb = codec_hs_ext_pa;
+}
+
 static void msm8x16_wcd_compute_impedance(struct snd_soc_codec *codec, s16 l,
 				s16 r, uint32_t *zl, uint32_t *zr, bool high)
 {
@@ -2852,6 +2862,10 @@ static const struct soc_enum iir2_inp1_mux_enum =
 static const struct snd_kcontrol_new ext_spk_mux =
 	SOC_DAPM_ENUM_VIRT("Ext Spk Switch Mux", ext_spk_enum);
 
+/* kumajaya: Use the same ext_spk_enum */
+static const struct snd_kcontrol_new ext_hs_mux =
+	SOC_DAPM_ENUM_VIRT("Ext Headset Switch Mux", ext_spk_enum);
+
 static const struct snd_kcontrol_new rx_mix1_inp1_mux =
 	SOC_DAPM_ENUM("RX1 MIX1 INP1 Mux", rx_mix1_inp1_chain_enum);
 
@@ -4243,6 +4257,10 @@ static const struct snd_soc_dapm_route audio_map[] = {
 	{"Ext Spk Switch", "On", "HPHL PA"},
 	{"Ext Spk Switch", "On", "HPHR PA"},
 
+	{"Ext Headset", NULL, "Ext Headset Switch"},
+	{"Ext Headset Switch", "On", "HPHL PA"},
+	{"Ext Headset Switch", "On", "HPHR PA"},
+
 	{"HPHL PA", NULL, "HPHL"},
 	{"HPHR PA", NULL, "HPHR"},
 	{"HPHL", "Switch", "HPHL DAC"},
@@ -4707,6 +4725,32 @@ static int msm8x16_wcd_codec_enable_spk_ext_pa(struct snd_soc_dapm_widget *w,
 	return 0;
 }
 
+static int msm8x16_wcd_codec_enable_hs_ext_pa(struct snd_soc_dapm_widget *w,
+		struct snd_kcontrol *kcontrol, int event)
+{
+	struct snd_soc_codec *codec = w->codec;
+	struct msm8x16_wcd_priv *msm8x16_wcd = snd_soc_codec_get_drvdata(codec);
+
+	dev_dbg(codec->dev, "%s: %s event = %d\n", __func__, w->name, event);
+	switch (event) {
+	case SND_SOC_DAPM_POST_PMU:
+		dev_dbg(w->codec->dev,
+			"%s: enable external headset PA\n", __func__);
+		if (msm8x16_wcd->codec_hs_ext_pa_cb) {
+			msm8x16_wcd->codec_hs_ext_pa_cb(codec, 1);
+		}
+		break;
+	case SND_SOC_DAPM_PRE_PMD:
+		dev_dbg(w->codec->dev,
+			"%s: enable external headset PA\n", __func__);
+		if (msm8x16_wcd->codec_hs_ext_pa_cb) {
+			msm8x16_wcd->codec_hs_ext_pa_cb(codec, 0);
+		}
+		break;
+	}
+	return 0;
+}
+
 static int msm8x16_wcd_codec_enable_ear_pa(struct snd_soc_dapm_widget *w,
 	struct snd_kcontrol *kcontrol, int event)
 {
@@ -4792,6 +4836,8 @@ static const struct snd_soc_dapm_widget msm8x16_wcd_dapm_widgets[] = {
 
 	SND_SOC_DAPM_SPK("Ext Spk", msm8x16_wcd_codec_enable_spk_ext_pa),
 
+	SND_SOC_DAPM_SPK("Ext Headset", msm8x16_wcd_codec_enable_hs_ext_pa),
+
 	SND_SOC_DAPM_OUTPUT("HEADPHONE"),
 	SND_SOC_DAPM_PGA_E("HPHL PA", MSM8X16_WCD_A_ANALOG_RX_HPH_CNP_EN,
 		5, 0, NULL, 0,
@@ -4840,6 +4886,9 @@ static const struct snd_soc_dapm_widget msm8x16_wcd_dapm_widgets[] = {
 
 	SND_SOC_DAPM_VIRT_MUX("Ext Spk Switch", SND_SOC_NOPM, 0, 0,
 		&ext_spk_mux),
+
+	SND_SOC_DAPM_VIRT_MUX("Ext Headset Switch", SND_SOC_NOPM, 0, 0,
+		&ext_hs_mux),
 
 	SND_SOC_DAPM_MIXER("RX1 MIX1", SND_SOC_NOPM, 0, 0, NULL, 0),
 	SND_SOC_DAPM_MIXER("RX2 MIX1", SND_SOC_NOPM, 0, 0, NULL, 0),
